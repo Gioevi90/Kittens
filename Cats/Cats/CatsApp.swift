1eae42d2a74583510eb93447a4b8c5ba85aafb8c
Gioevi90 <gioevi90@gmail.com>
2022-02-19 11:53:07 +0100

    chore: first commit

---
+++ Cats/Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Giovanni Catania on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
